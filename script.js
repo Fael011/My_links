57ac3b637b9b838d2868bc4deb9915e7869bc471
Fael011 <rlimaviana011@hotmail.com>
2024-05-01 20:04:32 -0300

    Modo claro e modo escuro finalizado

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-const body =document.querySelector("body")
-const div = document.querySelector("div")
-const strong =document.querySelector("strong")
-const h1 = document.querySelector('h1')
-const p = document.querySelector('p')
-const link = document.querySelector('a')
+const body = document.querySelector("body")
 
-const botaoLight = document.getElementById("lightMode")
-const  botaoDark = document.getElementById("darkMode")
-const botoesLinks = document.querySelector("li")
+function toggleMode(){
 
-botaoLight.addEventListener('click', modoClaro)
-botaoDark.addEventListener('click', modoEscuro)
+    /*if(body.classList.contains("light")){
+        body.classList.remove("light")
+    }else{
+        body.classList.add("light")
+    }*/
 
-function modoClaro() {
-    body.classList.add("light")
-    div.classList.add("light")
-    h1.classList.add("light")
-    strong.classList.add("light")
-    p.classList.add("light")
-    link.classList.add("light")
-    botoesLinks.classList.add("light")
-}
-
-function modoEscuro(){
-    body.classList.remove("light")
-    div.classList.remove("light")
-    h1.classList.remove("light")
-    strong.classList.remove("light")
-    p.classList.remove("light")
-    link.classList.remove("light")
-    strong.classList.remove("light")
-    botoesLinks.classList.remove("light")
+    body.classList.toggle("light")
 }
